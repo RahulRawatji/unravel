54bf1f33995ef48102a3b8306439e546de93d61b
Rahul Rawat <93177337+RahulRawatji@users.noreply.github.com>
2025-06-23 16:11:50 +0000

    sample data hosted on firebase

--- src/api/fetchRoomData.jsx
+++ src/api/fetchRoomData.jsx
@@ -1,5 +1,5 @@
 async function fetchRoomData(){
-    const data =  await fetch('../sampleData/sampleData.json').then(data=>data.json()).then(data=>data);
+    const data =  await fetch('https://firebasestorage.googleapis.com/v0/b/travelai-d321b.appspot.com/o/sample.json?alt=media&token=981c8cbb-6750-4347-a82a-a5cd46ab21f4').then(data=>data.json()).then(data=>data);
     setRoomsData(data.rooms_by_serial_no[0].rooms);
     const tD = []
     for(let i=0;i<=10;i++){
